30d952cef39263c0821eec5452b23d3ee22dc992
chingan <chingan@192.168.11.121>
2017-10-02 23:05:41 +0800

    create heap sort.

---
+++ constructure_lotsOfSorts/src/heapSort/TestHeapSort.java
@@ -0,0 +1,16 @@
+package heapSort;
+
+public class TestHeapSort {
+
+	public static void main(String[] args) {
+		int[] test= {5, 19, 10, 2, 1, 3, 2, 7, 2};
+		
+
+	}
+	
+	//Adjust as a max heap.
+	public static void adjust() {
+		
+	}
+
+}
